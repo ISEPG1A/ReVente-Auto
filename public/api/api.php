11df1d3cb2a879508a14827a64e1e619a6949375
Marouane <marouanepng@gmail.com>
2025-11-07 10:00:30 +0100

    erge

---
+++ public/api/api.php
@@ -0,0 +1,3 @@
+<?php
+// Wrapper public pour l'API principale située hors du DocumentRoot
+require __DIR__ . '/../../api/api.php';
---
+++ public/api/auth.php
@@ -0,0 +1,3 @@
+<?php
+// Wrapper public pour auth situé hors du DocumentRoot
+require __DIR__ . '/../../api/auth.php';
