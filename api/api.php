f504173f15b96a635115d01692c132f1d29ee157
antoineperez314 <antoinesampayo@gmail.com>
2025-11-10 11:31:49 +0100

    Update api.php

--- api/api.php
+++ api/api.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * API de gestion des véhicules (CRUD)g
+ * API de gestion des véhicules (CRUD)
  * 
  * Points d'accès :
  * - GET  : Récupérer la liste des véhicules (avec recherche optionnelle)
